4c9b737531094ed403360e38c9a95caf710314c1
ahenriquez <ahenriquez@eficent.com>
2020-01-14 16:42:55 +0100

    [MIG]rma to v13

--- rma/wizards/stock_config_settings.py
+++ rma/wizards/stock_config_settings.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright (C) 2017 ForgeFlow
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from odoo import fields, models
@@ -10,12 +10,12 @@ class StockConfigSettings(models.TransientModel):
     group_rma_delivery_address = fields.Selection(
         [
             (
-                0,
+                "0",
                 "Invoicing and shipping addresses are always the same "
                 "(Example: services companies)",
             ),
             (
-                1,
+                "1",
                 "Display 3 fields on rma: partner, invoice address, delivery "
                 "address",
             ),
@@ -25,7 +25,8 @@ class StockConfigSettings(models.TransientModel):
     )
 
     group_rma_lines = fields.Selection(
-        [(0, "Do not group RMA lines"), (1, "Group RMA lines in one RMA group")],
+        [("0", "Do not group RMA lines"),
+         ("1", "Group RMA lines in one RMA group")],
         "Grouping",
         implied_group="rma.group_rma_groups",
     )
